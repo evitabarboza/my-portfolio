b3f0973d975bf0739ce80a2262da19af8bcc2a33
evitabarboza <evitabarboza195@gmail.com>
2025-02-19 23:04:23 +0530

    Updated Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
           />
         </a>
 
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? " " :  "bg-white shadow-sm bg-opacity-50"} `}>
           <li>
             <a className='font-Ovo' href="#top">Home</a>
           </li>
